61a8d4bc6523b145a5f1d0c7f4a283b2b1f511bb
Dilshat Salikhov <dilschat@yandex.ru>
2018-03-30 23:24:50 +0300

    Extended bug fixed

--- UI.py
+++ UI.py
@@ -27,18 +27,17 @@ class UI(QMainWindow):
         self.button3 = QPushButton('Runge-Kutta method', self)
         self.button4 = QPushButton('Exit', self)
         self.button5 = QPushButton('Real solution', self)
+        self.button6 = QPushButton('Global trancation error', self)
         self.button1.move(50, 50)
         self.button2.move(50, 80)
         self.button2.resize(200, 32)
         self.button3.move(50, 110)
         self.button3.resize(200, 32)
-        self.button4.move(50, 170)
+        self.button4.move(50, 200)
         self.button5.resize(200, 32)
         self.button5.move(50, 140)
-        self.text_area = QPlainTextEdit(self)
-        self.text_area.resize(100, 50)
-        self.text_area.move(300, 50)
-        self.text_area.insertPlainText('Print size of step here')
+        self.button6.resize(200, 32)
+        self.button6.move(50, 170)
 
 
 class methods_UI(QMainWindow):
@@ -52,25 +51,49 @@ class methods_UI(QMainWindow):
         self.window = main_window
         self.recalculate_button = QPushButton('Recalculate', self)
         self.text_area = QPlainTextEdit(self)
-        self.text_area.move(500, 500)
+        self.text_area.move(100, 500)
         self.text_area.insertPlainText('new step value')
-        self.recalculate_button.move(800, 500)
+        self.text_area_x0 = QPlainTextEdit(self)
+        self.text_area_x0.move(200, 500)
+        self.text_area_x0.insertPlainText('X0')
+        self.text_area_y0 = QPlainTextEdit(self)
+        self.text_area_y0.move(300, 500)
+        self.text_area_y0.insertPlainText('Y0')
+        self.text_area_xend = QPlainTextEdit(self)
+        self.text_area_xend.move(400, 500)
+        self.text_area_xend.insertPlainText('X End point')
+        self.recalculate_button.move(700, 500)
         self.recalculate_button.resize(200, 32)
         self.step = STANDARD_STEP
+        self.x0 = START_SEGMENT
+        self.y0 = Y_START_POINT
+        self.x_end = END_SEGMENT
         self.recalculate_button.clicked.connect(self.recalculation)
 
     def initUI(self):
-        text = self.window.text_area.toPlainText()
+
+        text = self.text_area.toPlainText()
+        text_x0 = self.text_area_x0.toPlainText()
+        text_y0 = self.text_area_y0.toPlainText()
+        text_xends = self.text_area_xend.toPlainText()
+
         if self.is_number(text):
             self.step = float(text)
+        if self.is_number(text_x0):
+            self.x0 = float(text_x0)
+        if self.is_number(text_y0):
+            self.y0 = float(text_y0)
+        if self.is_number(text_xends):
+            self.x_end = float(text_xends)
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         m = PlotCanvas(self, width=5, height=4)
-        m.plot(self.title + ' method', self.step)
+
+        m.plot(self.title + ' method', self.step, self.x0, self.y0, self.x_end)
         m.move(0, 0)
 
-        if "Real" not in self.title:
+        if "Real" not in self.title and "Global" not in self.title:
             error_canvas = PlotCanvas(self, width=5, height=4)
             error_canvas.plot_error(self.title + ' method error', m.solution)
             error_canvas.move(500, 0)
@@ -80,7 +103,6 @@ class methods_UI(QMainWindow):
     def is_number(self, s):
         try:
             float(s)
-            print(s)
             return True
         except ValueError:
             return False
@@ -106,30 +128,37 @@ class PlotCanvas(FigureCanvas):
 
         FigureCanvas.updateGeometry(self)
 
-    def plot(self, title, step):
-        solver = methods.Solver(START_SEGMENT, Y_START_POINT, END_SEGMENT)
-        real_solution = solver.real_solution()
+    def plot(self, title, step, x0, y0, x_end):
+
+        solver = methods.Solver(x0, y0, x_end)
+        real_solution = solver.calculate_real_solution()
         if "Euler" in title:
-            self.solution = solver.euler_method(step)
+            self.solution = solver.calculate_by_euler_method(step)
         elif "improved" in title:
-            self.solution = solver.improved_euler_method(step)
+            self.solution = solver.calculate_by_improved_euler_method(step)
         elif "Runge-kutta" in title:
-            self.solution = solver.runge_kutta_method(step)
+            self.solution = solver.calculate_by_runge_kutta_method(step)
         elif "Real" in title:
-            self.solution = solver.real_solution()
+            self.solution = solver.calculate_real_solution()
+        elif "Global" in title:
+            self.solution = solver.calculate_global_error()
         else:
             raise IOError
         main_plot = self.figure.add_subplot(111)
-
-        main_plot.plot(self.solution[0], self.solution[1], label=title)
-        main_plot.plot(real_solution[0], real_solution[1], label='real solution')
+        if "Global" not in title:
+            main_plot.plot(real_solution[0], real_solution[1], label='real solution')
+            main_plot.plot(self.solution[0], self.solution[1], label=title)
+        else:
+            main_plot.plot(self.solution[0], self.solution[1]['euler'], label='euler error')
+            main_plot.plot(self.solution[0], self.solution[1]['improved euler'], label='improved euler error')
+            main_plot.plot(self.solution[0], self.solution[1]['runge-kutta'], label='runge-kutta')
         main_plot.legend(loc='upper left')
         main_plot.set_title(title)
         self.draw()
 
     def plot_error(self, title, solution):
         solver = methods.Solver(START_SEGMENT, Y_START_POINT, END_SEGMENT)
-        error = solver.calculate_error(solution[0], solution[1])
+        error = solver.calculate_local_error(solution[0], solution[1])
         main_plot = self.figure.add_subplot(111)
         main_plot.plot(error[0], error[1])
         main_plot.set_title(title)
@@ -141,12 +170,14 @@ if __name__ == "__main__":
     myWidget = UI()
     euler_ui = methods_UI('Euler', myWidget)
     improved_euler_ui = methods_UI('Improved Euler', myWidget)
-    runge_kutta_ui = methods_UI('Runge-kutta',  myWidget)
+    runge_kutta_ui = methods_UI('Runge-kutta', myWidget)
     real_solution_ui = methods_UI('Real solution', myWidget)
+    global_error_ui = methods_UI('Global trancation error', myWidget)
     myWidget.button1.clicked.connect(euler_ui.initUI)
     myWidget.button2.clicked.connect(improved_euler_ui.initUI)
     myWidget.button3.clicked.connect(runge_kutta_ui.initUI)
     myWidget.button5.clicked.connect(real_solution_ui.initUI)
+    myWidget.button6.clicked.connect(global_error_ui.initUI)
     myWidget.button4.clicked.connect(app.exit)
     myWidget.show()
     app.exec_()
--- numeric_methods.py
+++ numeric_methods.py
@@ -7,12 +7,15 @@ class Solver(object):
         self.y0 = y0
         self.X = X
         self.c = self.calculateConstant(self.x0, self.y0)
-    def real_solution(self):
+
+    def calculate_real_solution(self):
+        """ This function calculates x and y values for exact solution of given diff.equation"""
         x = numpy.linspace(self.x0, self.X, 100)
         y = self.calculate_function(x, self.c)
         return [x, y]
 
-    def euler_method(self, h):
+    def calculate_by_euler_method(self, h):
+        """ This function calculates approximated solution of given diff. equation using euler method """
         list_of_x = [self.x0]
         list_of_y = [self.y0]
         last_x = list_of_x[-1]
@@ -23,7 +26,8 @@ class Solver(object):
             list_of_x.append(last_x + h)
         return [list_of_x, list_of_y]
 
-    def improved_euler_method(self, h):
+    def calculate_by_improved_euler_method(self, h):
+        """ This function calculates approximated solution of given diff. equation using improved euler method """
         list_of_x = [self.x0]
         list_of_y = [self.y0]
         last_x = list_of_x[-1]
@@ -36,7 +40,8 @@ class Solver(object):
             list_of_x.append(last_x + h)
         return [list_of_x, list_of_y]
 
-    def runge_kutta_method(self, h):
+    def calculate_by_runge_kutta_method(self, h):
+        """ This function calculates approximated solution of given diff. equation using runge-kutta method """
         list_of_x = [self.x0]
         list_of_y = [self.y0]
         last_x = list_of_x[-1]
@@ -52,16 +57,42 @@ class Solver(object):
         return [list_of_x, list_of_y]
 
     def calculate_derivative(self, x, y):
+        """ Calculates value of derivative for given d.e."""
         return x * numpy.power(y, 1.5) + x * y
 
     def calculate_function(self, x, c):
+        """ Calculates value of exact solution of d.e."""
         return numpy.power(numpy.e, (numpy.power(x, 2) / 2)) / numpy.power((
             c - numpy.power(numpy.e, numpy.power(x, 2) / 4)), 2)
 
     def calculateConstant(self, x0, y0):
-        return numpy.power(numpy.e, numpy.power(x0, 2)/4)+numpy.power(numpy.power(numpy.e, numpy.power(x0, 2)/2)/y0, 0.5)
+        """ Calculates constant value for solution of d.e."""
+        return numpy.power(numpy.e, numpy.power(x0, 2) / 4) + numpy.power(
+            numpy.power(numpy.e, numpy.power(x0, 2) / 2) / y0, 0.5)
 
-    def calculate_error(self, x, y):
-        error_y = list()
+    def calculate_local_error(self, x, y):
+        """ Calculates local trancation error """
         error_y = numpy.abs(numpy.subtract(y, self.calculate_function(x, self.c)))
         return [x, error_y]
+
+    def calculate_global_error(self):
+        """ Calculates global trancation error"""
+        x = numpy.linspace(0.0001, 0.2, 3000)
+        euler_solution = list()
+        improved_euler_solution = list()
+        runge_kutta_solution = list()
+        for i in x:
+            euler_solution.append(self.calculate_by_euler_method(i))
+            improved_euler_solution.append(self.calculate_by_improved_euler_method(i))
+            runge_kutta_solution.append(self.calculate_by_runge_kutta_method(i))
+        euler_error = self.calculate_max_local_error(euler_solution)
+        improved_euler_error = self.calculate_max_local_error(improved_euler_solution)
+        runge_cutta_error = self.calculate_max_local_error(runge_kutta_solution)
+        dictionary = {'euler': euler_error, 'improved euler': improved_euler_error, 'runge-kutta': runge_cutta_error}
+        return [x, dictionary]
+
+    def calculate_max_local_error(self,solution):
+        error = list()
+        for i in solution:
+            error.append(max((self.calculate_local_error(i[0], i[1]))[1]))
+        return error
